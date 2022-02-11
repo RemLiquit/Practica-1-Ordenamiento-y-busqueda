c060b51707c65baa1cf5360c10c985d1be59c268
Diego <ceixpp@gmail.com>
2022-02-11 14:15:29 -0400

    first
    terminado

---
+++ Practica-Ordenamiento-Busqueda/Practica-Ordenamiento-Busqueda/Persona.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica_Ordenamiento_Busqueda
+{
+  class Persona
+  {	
+	public int[] vector = new int[0];
+	public string[] vn = new string[0];
+
+	public void Nombre(int n)
+	{
+	  n = n - 1;
+	  for (int k = 0;k < n;k++)
+	  {
+		for (int f = 0;f < n - k;f++)
+		{
+		  if (vn[f].CompareTo(vn[f + 1]) > 0)
+		  {
+			string aux;
+			int aux2 = 0;
+			aux2 = vector[f];
+			aux = vn[f];
+			vn[f] = vn[f + 1];
+			vn[f + 1] = aux;
+			vector[f] = vector[f + 1];
+			vector[f + 1] = aux2;
+		  }
+		}
+	  }
+	  //Arreglo ordenado por edad
+	  Console.WriteLine("Vector Ordenado");
+	  for (int i = 0;i < n+1;i++)
+	  {
+		Console.Write(i + " ");
+		Console.Write(vn[i] + " ");
+		Console.WriteLine(vector[i]);
+	  }
+
+	}
+
+	public void Edad(int n)
+	{
+	  for (int i = n - 1;i > 0;i--)
+	  {
+		for (int j = 0;j < i;j++)
+		{
+		  if (vector[j] > vector[j + 1])
+		  {
+			string aux;
+			int aux2 = 0;
+			aux2 = vector[j];
+			aux = vn[j];
+			vn[j] = vn[j + 1];
+			vn[j + 1] = aux;
+			vector[j] = vector[j + 1];
+			vector[j + 1] = aux2;
+		  }
+		}
+	  }
+	  //Arreglo ordenado por edad
+	  Console.WriteLine("Vector Por edad");
+	  for (int i = 0;i < n;i++)
+	  {
+		Console.Write(i + " ");
+		Console.Write(vn[i] + " " );
+		Console.WriteLine(vector[i]);
+	  }
+	}
+	public void Menor(int n)
+	{
+	  int aux=0;
+	  int menor=vector[0];
+	  for (int i=0;i < n;i++)
+	  {
+		if (vector[i]< menor)
+		{
+		  menor = vector[i];
+		  aux = i;
+		}
+	  }
+	  Console.WriteLine("La persona con menor edad es");
+	  Console.WriteLine(vn[aux] + " " + menor);
+	}
+  }
+}
---
+++ Practica-Ordenamiento-Busqueda/Practica-Ordenamiento-Busqueda/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Practica_Ordenamiento_Busqueda
+{
+  class Program
+  {	
+	static void Main(string[] args)
+	{
+	  Persona nPersona = new Persona();
+	  string nombre;
+	  int edad;
+	  Console.WriteLine("Cuantos nombres desea ingresar?");	  
+	  int tam = int.Parse(Console.ReadLine());	  
+	  int[] ve = new int[tam];
+	  string[] vn = new string[tam];
+	  
+	  for (int i=0; i<tam;i++)
+	  {
+		try
+		{
+		  Console.WriteLine("Ingresando " + (i + 1) + "º nombre");
+		  nombre = Console.ReadLine();
+		  vn[i] = nombre;
+		  Console.WriteLine("Ingresando " + (i + 1) + "º edad");
+		  edad = int.Parse(Console.ReadLine());
+		  ve[i] = edad;
+		}
+		catch (Exception x)
+		{
+		  Console.WriteLine(x);
+		}
+		finally
+		{
+		  nPersona.vn = vn;
+		  nPersona.vector = ve;
+		}
+	  }	  
+	  nPersona.Menor(tam);	  
+	  nPersona.Nombre(tam);	  
+	  nPersona.Edad(tam);
+	  Console.WriteLine("");
+	  Console.WriteLine("Ordenamiento");	  
+	  Random nRandom = new Random();	  
+	  int[] vector = new int[10];
+	  //Rellenado numeros random
+	  for (int i = 0;i < 10;i++)
+	  {
+		vector[i] = nRandom.Next(0,9);
+	  }
+	  Burbuja(vector);
+	}
+
+	static void Burbuja(int[] vector)
+	{
+	  int aux = 0;
+	  Console.WriteLine("Impresion del vector");
+	  //Impresion vector
+	  for (int i = 0;i < 10;i++)
+	  {
+		Console.Write(vector[i] + ";");
+	  }
+	  Console.WriteLine("");
+	  //Metodo Burbuja
+	  for (int i = 10 - 1;i > 0;i--)
+	  {
+		for (int j = 0;j < i;j++)
+		{
+		  if (vector[j] > vector[j + 1])
+		  {
+			aux = vector[j];
+			vector[j] = vector[j + 1];
+			vector[j + 1] = aux;
+		  }
+		}
+	  }
+	  //Vector Ordenado
+	  Console.WriteLine("Vector Ordenado");
+	  for (int i = 0;i < 10;i++)
+	  {
+		Console.Write(vector[i] + ";");
+	  }
+	}
+
+  }
+}
